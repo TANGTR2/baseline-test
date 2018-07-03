f10ec66d960108619aa0863e4e236bbcf09d8ad7
TANGTR2 <40784777+TANGTR2@users.noreply.github.com>
2018-07-03 14:40:03 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
+  for(var i=0;i<collectionA.length;i++){
+    for(var j=0;j<collectionB.length;j++){
+      if(i==j)
+        document.write(i + "<br>");
+      else continue;
+    }
+  }
   return '实现练习要求，并改写该行代码。';
 }
