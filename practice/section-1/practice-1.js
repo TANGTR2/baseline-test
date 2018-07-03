8fb8827b0988d59c383fe887fe9cab52e42b4500
TANGTR2 <40784777+TANGTR2@users.noreply.github.com>
2018-07-03 14:46:19 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,11 +2,11 @@
 
 function collectSameElements(collectionA, collectionB) {
   for(var i=0;i<collectionA.length;i++){
-    for(var j=0;j<collectionB.length;j++){
-      if(i==j)
-        document.write(i + "<br>");
-      else continue;
-    }
-  }
+		    for(var j=0;j<collectionB.length;j++){
+		      if(collectionA[i]==collectionB[j])
+		        document.write(collectionA[i] + "<br>");
+		      else continue;
+		    }
+	}
   return '实现练习要求，并改写该行代码。';
 }
